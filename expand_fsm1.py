c553fb476bdb64ac0a0b2c30d970b9557cf6b31b
EJ <ejsim@uwalumni.com>
2017-10-23 23:55:19 -0500

    first

---
+++ expand_fsm1.py
@@ -0,0 +1,36 @@
+# Erica Sim
+# Ling 570 Autumn 2017
+# hw4
+
+import sys
+
+#making an FST (in Carmel format) for the lexicon
+LEXICON = open(sys.argv[1], "r")
+#LEXICON = open("lexicon_ex", "r")
+
+# prints first line of output FST
+print("F")
+st_counter = 0
+
+for line in LEXICON:
+    if line == "\n":
+        continue
+
+    word = line.split()[0]
+    pos_tag = line.split()[1]
+
+    for letter in word:
+        if len(word) == 1:
+            print('(q0 (F "' + letter + '" ' + pos_tag + '))')
+
+        elif letter is word[0]:
+            print('(q0 (q' + str(st_counter + 1) + ' "' + letter + '" *e*))')
+
+        #last letter
+        elif letter is word[len(word)-1]:
+            print('(q' + str(st_counter) + ' (F "' + letter + '" ' + pos_tag + '))')
+
+        else:
+            print('(q' + str(st_counter) + ' (q' + str(st_counter + 1) + ' "' + letter + '" *e*))')
+
+        st_counter += 1
