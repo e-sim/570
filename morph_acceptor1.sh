3e57833619d0c2fca222d6e407c1c39dd108c18d
EJ <ejsim@uwalumni.com>
2017-10-25 21:59:57 -0500

    part 2 works!

--- morph_acceptor1.sh
+++ morph_acceptor1.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 #author: Erica Sim
+# $1 = fsm file $2 = word list
 
 while read curr; do
-    echo $curr | carmel -sliq $1 2>&1
-	#RESULT="$(echo $curr | carmel -sliq $1 2>&1 | tail -n 1)"
-	#if [[ $RESULT =~ .*Empty.* ]] #stderr says "Empty or invalid..." w/ no match
-	#then
-	#	echo "$curr => no" 
-	#else
-	#	echo "$curr => yes"
-	#fi
+   
+	RESULT="$(echo $curr | sed -e 's/\(.\)/\1 /g' | carmel -sli $1 2>&1)"
+	if [[ $RESULT =~ .*Empty.* ]] #stderr says "Empty or invalid..." w/ no match
+	then
+        #echo $RESULT
+		echo "$curr => no" 
+	else
+        #echo $RESULT
+		echo "$curr => yes"
+	fi
 done < $2
